b3453a4e566df48144029a187f1a248ba5ce9a2e
Hassan <hassan.anvery@gmail.com>
2026-01-16 09:22:49 -0800

    Add initial Xcode iOS app project

---
+++ TennisChartingApp/TennisChartingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TennisChartingApp
+//
+//  Created by Syed Hassan Ali Anvery on 1/15/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TennisChartingApp/TennisChartingApp/TennisChartingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisChartingAppApp.swift
+//  TennisChartingApp
+//
+//  Created by Syed Hassan Ali Anvery on 1/15/2026.
+//
+
+import SwiftUI
+
+@main
+struct TennisChartingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
